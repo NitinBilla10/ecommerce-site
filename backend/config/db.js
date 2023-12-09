88ecb8bc659cad020ae4188a5567aa12e237c472
Nitin Billa <nitinbilla3@gmail.com>
2023-12-10 01:28:03 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -9,7 +9,7 @@ const connectDB = async ()=>{
         const conn = await mongoose.connect(process.env.MONGO_URL)
         console.log("MongoDB Connected")}
     catch(err){
-        console.log("Error", e)
+        console.log("Error", err)
     }
 
 
